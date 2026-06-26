dea76197479c38458ba231fd95e08ab7226ce026
kyloren831 <aaronmc080301@hotmail.com>
2026-06-26 15:29:50 -0600

    crear usuario

--- ProyectoEcommerce/views/login_view.py
+++ ProyectoEcommerce/views/login_view.py
@@ -3,8 +3,6 @@ from tkinter import ttk
 from utils.ui import card
 
 
-
-
 class LoginView(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent, style="Main.TFrame")
